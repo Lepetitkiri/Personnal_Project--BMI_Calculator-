570b5fcc5d1e8b00e6152d4f2ac5763107de8476
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2024-02-21 16:18:20 +0100

    Header added

--- src/Components/Header/Index.jsx
+++ src/Components/Header/Index.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 import HeaderStyle from "./Style.jsx";
 
 function Header() {
   return (
     <HeaderStyle>
-      <Link to="/"> Retour vers Accueil </Link>
+      <h1>Calculez votre IMC en 2 clics</h1>
     </HeaderStyle>
   );
 }
--- src/Components/Header/Style.jsx
+++ src/Components/Header/Style.jsx
@@ -8,14 +8,23 @@ const HeaderStyle = styled.header`
   justify-content: center;
   height: 100px;
   width: 100%;
-  background-color: ${colors.primary};
+  background-color: ${colors.tertiary};
+  font-size: large;
+  font-family: Montserrat,'Courier New',Courier,sans-serif;
+
+  h1 {
+    color: ${colors.black};
+    text-align: center;
+  }
 
   /* Version tablette */
   @media only screen and (max-width: 992px) {
+    font-size: small;
   }
 
   /* Version mobile */
   @media only screen and (max-width: 767px) {
+    font-size: x-small;
   }
 `;
 
